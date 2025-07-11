85e17d4c80dc027120d4ed6f92978893d77e5b15
architector1324 <olegsajaxov@yandex.ru>
2025-07-11 12:45:58 +0300

    cli: boilerplate + run command run: simple llm inference

--- src/vkllama.py
+++ src/vkllama.py
@@ -1,1 +1,26 @@
-import llama_cpp
+import random
+import argparse
+import vkllama_run
+
+
+# main
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Vulkan LLM tool', add_help=False)
+    subparsers = parser.add_subparsers(dest='command', help='Commands')
+    parser.add_argument('--help', action='help')
+
+    # run
+    run_parser = subparsers.add_parser('run', help='Run a model', add_help=False)
+    run_parser.add_argument('-m', '--model', type=str, default=vkllama_run.DEFAULT_MODEL, help='Model')
+    run_parser.add_argument('--seed', default=random.randint(0, 2**32 - 1), type=int, help='Specify a numerical seed for reproducible text generation. If not provided, a random seed will be used.')
+    run_parser.add_argument('-s', '--stream', default=None, type=int, help='Stream steps to output')
+    run_parser.add_argument('-t', '--think', action='store_true', help='Enable advanced, iterative reasoning for the model to refine outputs. May increase processing time and token usage.')
+    run_parser.add_argument('prompt', nargs='+', type=str, help='Prompt for model')
+    run_parser.add_argument('--help', action='help')
+
+    args = parser.parse_args()
+
+    if args.command == 'run':
+        vkllama_run.run(args)
+    else:
+        parser.print_help()
---
+++ src/vkllama_run.py
@@ -0,0 +1,50 @@
+import llama_cpp
+
+
+DEFAULT_MODEL = 'gemma3'
+
+MODELS = [
+    {
+        'name': 'gemma3',
+        'path': '/home/arch/AI/models/llm/gemma-3-4b-it-Q4_K_M.gguf'
+    },
+    {
+        'name': 'qwen3:4b',
+        'path': '/home/arch/AI/models/llm/Qwen3-4B-Q4_K_M.gguf'
+    }
+]
+
+def run(args):
+    # get prompt and model
+    prompt = ' '.join(args.prompt)
+    model = next((e for e in MODELS if e['name'] == args.model), None)
+
+    if not model:
+        print(f'error: model "{args.model}" not found!')
+        return
+
+    # init
+    llm = llama_cpp.Llama(
+        model_path=model['path'],
+        n_gpu_layers=-1,
+        n_ctx=2048,
+        verbose=False
+    )
+
+    out = llm.create_chat_completion(
+        messages=[
+            {
+                'role': 'user',
+                'content': prompt
+            }
+        ],
+        # max_tokens=512,
+        stream=True
+    )
+
+    # run
+    for msg in out:
+        if 'content' in msg['choices'][0]['delta']:
+            txt = msg['choices'][0]['delta']['content']
+            print(txt, end='', flush=True)
+    print()
