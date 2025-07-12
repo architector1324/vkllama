4ab0a195725795515097fc59461eeed4feed2243
architector1324 <olegsajaxov@yandex.ru>
2025-07-12 17:11:52 +0300

    cli: changed default port

--- src/vkllama.py
+++ src/vkllama.py
@@ -18,20 +18,20 @@ if __name__ == '__main__':
     run_parser.add_argument('--seed', default=random.randint(0, 2**32 - 1), type=int, help='Specify a numerical seed for reproducible text generation. If not provided, a random seed will be used.')
     run_parser.add_argument('-s', '--stream', action='store_true', help='Stream output')
     run_parser.add_argument('-t', '--think', action='store_true', help='Enable advanced, iterative reasoning for the model to refine outputs. May increase processing time and token usage.')
-    run_parser.add_argument('-a', '--address', default='0.0.0.0:11435', type=str, help='Server host address')
+    run_parser.add_argument('-a', '--address', default='0.0.0.0:11434', type=str, help='Server host address')
     run_parser.add_argument('prompt', nargs='+', type=str, help='Prompt for model')
     run_parser.add_argument('--help', action='help')
 
     # list
     list_parser = subparsers.add_parser('list', help='List available models', add_help=False)
-    list_parser.add_argument('-a', '--address', default='0.0.0.0:11435', type=str, help='Server host address')
+    list_parser.add_argument('-a', '--address', default='0.0.0.0:11434', type=str, help='Server host address')
     list_parser.add_argument('--help', action='help')
 
     # serve
     serve_parser = subparsers.add_parser('serve', help='Start LLM server', add_help=False)
     serve_parser.add_argument('--host', default='0.0.0.0', type=str, help='Server host address')
     serve_parser.add_argument('-m', '--models', default=vkllama_serve.DEFAULT_MODELS_PATH, type=str, help='LLM models path')
-    serve_parser.add_argument('-p', '--port', default=11435, type=int, help='Server port')
+    serve_parser.add_argument('-p', '--port', default=11434, type=int, help='Server port')
     # serve_parser.add_argument('-d', '--device', default=imagine_server_defs.DEFAULT_DEVICE, type=str,  choices=['cpu', 'cuda', 'mps'], help='Model compute device')
     serve_parser.add_argument('--help', action='help')
 
