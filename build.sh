57ce5d3d4c840ec181b5b471670a6d499b4f2e0b
architector1324 <olegsajaxov@yandex.ru>
2025-07-12 21:56:55 +0300

    build: fixed

--- build.sh
+++ build.sh
@@ -10,7 +10,19 @@ pip install pyinstaller requests
 CMAKE_ARGS="-DGGML_VULKAN=on" pip install llama-cpp-python
 
 cp ./src/*.py -t build/
-# cd build
-# CMAKE_ARGS="-DGGML_VULKAN=on" pyinstaller --onefile --collect-all llama-cpp-python --hidden-import vkllama_run --hidden-import vkllama_serve --hidden-import vkllama_list --add-data="vkllama_run.py:." --add-data="vkllama_list.py:." --add-data="vkllama_serve.py:." vkllama.py
+cd build
 
-# mv dist/vkllama ../vkllama
+mkdir -p ./venv/lib/python3.13/site-packages/llama_cpp/lib/
+cp ./venv/lib/python3.13/site-packages/lib/libllama.so ./venv/lib/python3.13/site-packages/llama_cpp/lib/
+
+pyinstaller --onefile --collect-all llama-cpp-python \
+    --hidden-import vkllama_run \
+    --hidden-import vkllama_serve \
+    --hidden-import vkllama_list \
+    --add-data="vkllama_run.py:." \
+    --add-data="vkllama_list.py:." \
+    --add-data="vkllama_serve.py:." \
+    --add-binary="./venv/lib/python3.13/site-packages/llama_cpp/lib/libllama.so:llama_cpp/lib" \
+    vkllama.py
+
+mv dist/vkllama ../vkllama
--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-source /home/arch/AI/Projects/vkllama/build/venv/bin/activate
-python /home/arch/AI/Projects/vkllama/src/vkllama.py serve
